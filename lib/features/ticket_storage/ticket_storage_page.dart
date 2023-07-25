a5f73b8671eb292d68df444c1815185c5fab5784
dashta <dashta>
2023-07-25 08:13:49 +0300

    Bloc and state

--- lib/features/ticket_storage/ticket_storage_page.dart
+++ lib/features/ticket_storage/ticket_storage_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:surf_flutter_study_jam_2023/features/screen/screen_tiket.dart';
+import 'package:surf_flutter_study_jam_2023/features/ticket_storage/screen_tiket.dart';
 
 /// Экран “Хранения билетов”.
 class TicketStoragePage extends StatelessWidget {
